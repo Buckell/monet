8fb5fe5f0b04c41253bc3047c03f2b84f9b0ac59
Max Goddard <buckell@alternus.dev>
2023-12-06 03:15:45 -0800

    Added default web panel port to definitions.

--- include/monet/definitions.hpp
+++ include/monet/definitions.hpp
@@ -12,6 +12,8 @@ namespace monet {
     constexpr size_t dmx_data_channel_count = 512;
     constexpr size_t dmx_universe_buffer_size = dmx_data_channel_count + 1;
 
+    constexpr uint16_t default_web_panel_port = 8080;
+
 }
 
 #endif //MASTER_SERVER_DEFINITIONS_HPP
