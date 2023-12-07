d5ebea44a6e3e9147f42a356ddd453f7541b77e9
Max Goddard <buckell@alternus.dev>
2023-12-06 20:00:39 -0800

    Implemented front-end forwarding to React development server.

--- src/monet/interface/web_panel.cpp
+++ src/monet/interface/web_panel.cpp
@@ -17,6 +17,16 @@ namespace monet::interface {
                 res.set_content("Hello, world!", "text/plain");
             });
 
+            m_server->Get(R"(^(?!/api).+$)", [](httplib::Request const& req, httplib::Response& res) {
+                std::string request_path{req.matches[0]};
+
+                // Forward request to front-end web panel server.
+                httplib::Client cli("http://localhost:3000");
+                auto panel_res = cli.Get(request_path);
+
+                res.set_content(panel_res->body, panel_res->get_header_value("Content-Type"));
+            });
+
             // ... Setup
 
             m_server->listen("0.0.0.0", m_port);
