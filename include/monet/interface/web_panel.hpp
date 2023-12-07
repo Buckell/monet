cbf56dd4b630953696394cd5ca5806152f0c692a
Max Goddard <buckell@alternus.dev>
2023-12-06 16:55:06 -0800

    Implemented and completed initial web panel server setup/framework.

--- include/monet/interface/web_panel.hpp
+++ include/monet/interface/web_panel.hpp
@@ -7,16 +7,34 @@
 
 #include <string>
 #include <thread>
+#include <memory>
+
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include <httplib.h>
+#undef interface
+
+#include "../definitions.hpp"
 
 namespace monet::interface {
+
     class web_panel {
         std::thread m_main_thread;
+
+        std::unique_ptr<httplib::SSLServer> m_server;
+
+        uint16_t m_port;
 
         std::string m_certificate_file;
         std::string m_private_key_file;
 
     public:
-        web_panel() : m_main_thread(), m_certificate_file(), m_private_key_file() {}
+        web_panel() :
+            m_main_thread(),
+            m_server(nullptr),
+            m_port(default_web_panel_port),
+            m_certificate_file(),
+            m_private_key_file()
+        {}
 
         web_panel(web_panel const&) = delete;
         web_panel(web_panel&&)      = delete;
@@ -39,9 +57,51 @@ namespace monet::interface {
         void stop();
 
         /**
-         *
+         * @brief Set the SSL certificate file.
          */
+        void set_ssl_certificate_file(std::string_view const a_certificate_file) noexcept {
+            m_certificate_file = a_certificate_file;
+        }
+
+        /**
+         * @brief Get the SSL certificate file.
+         */
+        [[nodiscard]]
+        std::string_view ssl_certificate_file() const noexcept {
+            return m_certificate_file;
+        }
+
+        /**
+         * @brief Set the SSL private key file.
+         */
+        void set_ssl_private_key_file(std::string_view const a_private_key_file) noexcept {
+            m_private_key_file = a_private_key_file;
+        }
+
+        /**
+         * @brief Get the SSL private key file.
+         */
+        [[nodiscard]]
+        std::string_view ssl_private_key_file() const noexcept {
+            return m_private_key_file;
+        }
+
+        /**
+         * @brief Set server port.
+         */
+        void set_port(uint16_t const a_port) noexcept {
+            m_port = a_port;
+        }
+
+        /**
+         * @brief Get server port.
+         */
+        [[nodiscard]]
+        uint16_t port() const noexcept {
+            return m_port;
+        }
     };
+
 }
 
 #endif //MASTER_SERVER_WEB_PANEL_HPP
--- src/monet/interface/web_panel.cpp
+++ src/monet/interface/web_panel.cpp
@@ -1,3 +1,30 @@
 //
 // Created by maxng on 12/6/2023.
 //
+
+#include <monet.hpp>
+
+namespace monet::interface {
+
+    void web_panel::start() {
+        m_main_thread = std::thread([&] {
+            m_server = std::make_unique<httplib::SSLServer>(
+                m_certificate_file.c_str(),
+                m_private_key_file.c_str()
+            );
+
+            m_server->Get("/test", [](httplib::Request const& req, httplib::Response& res) {
+                res.set_content("Hello, world!", "text/plain");
+            });
+
+            // ... Setup
+
+            m_server->listen("0.0.0.0", m_port);
+        });
+    }
+
+    void web_panel::stop() {
+        m_server->stop();
+    }
+
+}
