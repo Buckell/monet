08a108b39d095970473aaf1390d1069ab28812eb
Max Goddard <buckell@alternus.dev>
2023-12-11 00:57:17 -0800

    Added new headers to main header.

--- include/monet.hpp
+++ include/monet.hpp
@@ -5,6 +5,11 @@
 #ifndef MASTER_SERVER_MONET_HPP
 #define MASTER_SERVER_MONET_HPP
 
+#include "monet/channel/attribute/attribute.hpp"
+#include "monet/channel/attribute/intensity.hpp"
+#include "monet/channel/address_mapping.hpp"
+#include "monet/channel/attribute_definition.hpp"
+#include "monet/channel/configuration.hpp"
 #include "monet/dmx/master_id.hpp"
 #include "monet/dmx/universe.hpp"
 #include "monet/interface/web_panel.hpp"
