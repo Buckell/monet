ba589c11c6962bf76495d7071da77dd554fc7e36
Max Goddard <max@altis.gg>
2024-02-14 21:35:04 -0800

    Added test case for channels used in servers--value updating, shortcut functions, mapping, etc.

--- src/testing/channels.cpp
+++ src/testing/channels.cpp
@@ -179,3 +179,76 @@ TEST(Channels, AdvancedChannel) {
     EXPECT_EQ(values[6], 160);
     EXPECT_EQ(values[7], 80);
 }
+
+TEST(Channels, AddressOutput) {
+    monet::server server;
+    server.create_universe(1);
+
+    monet::channel::configuration config("Advanced Configuration");
+
+    config.add_attribute("intensity", monet::channel::attribute_definition("Intensity 1"));
+    config.add_attribute("rgb_color", monet::channel::attribute_definition("Color 1"));
+    config.add_attribute("rgb_color", monet::channel::attribute_definition("Color 2"));
+
+    auto& address_mappings = config.address_mappings();
+    address_mappings.emplace_back("intensity", 0);
+    address_mappings.emplace_back("rgb_color", 0, "red");
+    address_mappings.emplace_back("rgb_color", 0, "green");
+    address_mappings.emplace_back("rgb_color", 0, "blue");
+    address_mappings.emplace_back("rgb_color", 1, "blue");
+    address_mappings.emplace_back("rgb_color", 1, "green");
+    address_mappings.emplace_back("rgb_color", 1, "red");
+
+    auto& channel = server.create_channel(1, config, 1);
+
+    channel.attributes("intensity")[0]->set_value(monet::channel::attribute::intensity::base, 127);
+
+    auto const& rgb1 = channel.attributes("rgb_color")[0];
+    rgb1->set_value(monet::channel::attribute::rgb_color::red,   80);
+    rgb1->set_value(monet::channel::attribute::rgb_color::green, 160);
+    rgb1->set_value(monet::channel::attribute::rgb_color::blue,  240);
+
+    auto const& rgb2 = channel.attributes("rgb_color")[1];
+    rgb2->set_value(monet::channel::attribute::rgb_color::red,   240);
+    rgb2->set_value(monet::channel::attribute::rgb_color::green, 180);
+    rgb2->set_value(monet::channel::attribute::rgb_color::blue,  80);
+
+    channel.push_updates();
+
+    auto& universe = server.get_universe(1);
+    auto const* data = universe.buffer();
+
+    EXPECT_EQ(data[1], 127);
+
+    EXPECT_EQ(data[2], 80);
+    EXPECT_EQ(data[3], 160);
+    EXPECT_EQ(data[4], 240);
+
+    EXPECT_EQ(data[5], 80);
+    EXPECT_EQ(data[6], 180);
+    EXPECT_EQ(data[7], 240);
+
+    channel.set_intensity(100);
+
+    EXPECT_EQ(data[1], 255);
+
+    channel.set_rgb_color(255, 100, 30);
+
+    EXPECT_EQ(data[2], 255);
+    EXPECT_EQ(data[3], 100);
+    EXPECT_EQ(data[4], 30);
+
+    EXPECT_EQ(data[5], 80);
+    EXPECT_EQ(data[6], 180);
+    EXPECT_EQ(data[7], 240);
+
+    channel.set_rgb_color(0x30DFAC);
+
+    EXPECT_EQ(data[2], 48); // 0x30
+    EXPECT_EQ(data[3], 223); // 0xDF
+    EXPECT_EQ(data[4], 172); // 0xAC
+
+    EXPECT_EQ(data[5], 80);
+    EXPECT_EQ(data[6], 180);
+    EXPECT_EQ(data[7], 240);
+}
