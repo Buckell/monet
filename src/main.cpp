0907d097f677c66f5f54ca9008339c1540c4faef
Max Goddard <max@altis.gg>
2024-02-28 23:15:41 -0800

    Current trial case 2/28/24.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <ranges>
+#include <future>
+#include <thread>
 
 #include <monet.hpp>
 
@@ -13,7 +16,17 @@
 #include "ftxui/dom/elements.hpp"  // for text, hbox, separator, Element, operator|, vbox, border
 #include "ftxui/util/ref.hpp"  // for Ref
 
+#include <e131.h>
+
+std::string input() {
+    std::string command;
+    std::getline(std::cin, command);
+    return command;
+}
+
 int main() {
+    using namespace std::chrono_literals;
+
     monet::server server;
 
     auto& web_panel = server.web_panel_interface();
@@ -21,47 +34,81 @@ int main() {
     web_panel.set_ssl_private_key_file("cert/ca.key");
     web_panel.start();
 
-    using namespace monet::channel::attribute;
+    server.set_sink_interface(new monet::sink::sacn());
 
-    auto intensity_attr = registry::instantiate_attribute("intensity");
+    server.start();
+
+    server.create_universe(1);
+
+    auto& rgb_config = server.channel_configuration("rgb_light");
+    rgb_config.add_attribute("intensity", { "Intensity", {} });
+    rgb_config.add_attribute("rgb_color", { "Color", {} });
+
+    // TODO: Add better way to emplace address mappings.
+    rgb_config.address_mappings().emplace_back( "intensity" );
+    rgb_config.address_mappings().emplace_back( "rgb_color", 0, "red" );
+    rgb_config.address_mappings().emplace_back( "rgb_color", 0, "green" );
+    rgb_config.address_mappings().emplace_back( "rgb_color", 0, "blue" );
+
+    auto& dimmer_config = server.channel_configuration("dimmer");
+    dimmer_config.add_attribute("intensity", { "Intensity", {} });
+    dimmer_config.address_mappings().emplace_back("intensity", 0);
+
+    auto& relay_config = server.channel_configuration("relay");
+    relay_config.add_attribute("boolean", { "Power", {} });
+    relay_config.address_mappings().emplace_back("boolean", 0);
 
 
-    std::cout << bool(intensity_attr) << std::endl;
+    server.create_channel(1, "dimmer", 1); // Dimmer Channel 1
+    server.create_channel(2, "dimmer", 2); // Dimmer Channel 2
+    server.create_channel(3, "relay", 5);  // Shelly Channel 1
+    server.create_channel(4, "relay", 27); // Remote Control Channel 1
+    server.create_channel(5, "relay", 28); // Remote Control Channel 2
+    server.create_channel(6, "relay", 29); // Remote Control Channel 3
+    server.create_channel(7, "relay", 30); // Remote Control Channel 4
+    server.create_channel(8, "relay", 31); // Remote Control Channel 5
 
-    intensity_attr->set_value(intensity::base, 100);
-    dynamic_cast<intensity*>(intensity_attr.get())->set_intensity_value(100);
-    std::cout << static_cast<size_t>(intensity_attr->value(intensity::normal)) << std::endl;
+    //chn.attributes("intensity")[0]->set_value(monet::channel::attribute::intensity::base, 0);
+    //chn.push_updates();
 
+    //std::future<std::string> input_future = std::async(std::launch::async, input);
+//
+//
+    //
+    //uint8_t value_r = 0;
+    //uint8_t value_g = 0;
+    //uint8_t value_b = 0;
+//
+    //while (true) {
+    //    if (input_future.wait_for(1ms) == std::future_status::ready) {
+    //        std::stringstream ss(input_future.get());
+    //        std::string term;
+//
+    //        ss >> term;
+    //        value_r = std::stoi(term);
+//
+    //        ss >> term;
+    //        value_g = std::stoi(term);
+//
+    //        ss >> term;
+    //        value_b = std::stoi(term);
+//
+    //        input_future = std::async(std::launch::async, input);
+//
+    //        auto& u1 = server.get_universe(1);
+//
+    //        auto& rgb_attr = chn.attributes("rgb_color")[0];
+//
+    //        rgb_attr->set_value(monet::channel::attribute::rgb_color::red, value_r);
+    //        rgb_attr->set_value(monet::channel::attribute::rgb_color::green, value_g);
+    //        rgb_attr->set_value(monet::channel::attribute::rgb_color::blue, value_b);
+    //        chn.push_updates();
+    //    }
+//
+    //    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    //}
 
-    monet::channel::configuration led_config("LED Light");
-
-    led_config.add_attribute("intensity", monet::channel::attribute_definition("Intensity"));
-    led_config.add_attribute("intensity", monet::channel::attribute_definition("Intensity 2"));
-    led_config.add_attribute("rgb_color", monet::channel::attribute_definition("Color"));
-
-    auto& address_mappings = led_config.address_mappings();
-    address_mappings.emplace_back("intensity", 0);
-    address_mappings.emplace_back("intensity", 1);
-    address_mappings.emplace_back("rgb_color", 0, "red");
-    address_mappings.emplace_back("rgb_color", 0, "green");
-    address_mappings.emplace_back("rgb_color", 0, "blue");
-
-    monet::channel::channel led_channel(led_config);
-
-    led_channel.attributes("intensity")[0]->set_value(monet::channel::attribute::intensity::normal, 50);
-    led_channel.attributes("intensity")[1]->set_value(monet::channel::attribute::intensity::normal, 75);
-
-    auto& color_attr = led_channel.attributes("rgb_color")[0];
-    color_attr->set_value(monet::channel::attribute::rgb_color::red,   255);
-    color_attr->set_value(monet::channel::attribute::rgb_color::green, 127);
-    color_attr->set_value(monet::channel::attribute::rgb_color::blue,  0);
-
-    auto address_values = led_channel.fetch_address_values();
-
-    for (auto const value : address_values) {
-        std::cout << static_cast<size_t>(value) << " ";
-    }
-    std::cout << std::endl;
+    while (true);
 
     return EXIT_SUCCESS;
 
